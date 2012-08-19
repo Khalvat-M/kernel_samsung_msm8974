0b44296c1a6aa25aea9a2cd4ff2b6de1c0441b9b
Randy Dunlap <rdunlap@xenotime.net>
2012-08-18 17:45:08 -0700

    sched: Fix kernel-doc warnings in kernel/sched/fair.c
    Fix two kernel-doc warnings in kernel/sched/fair.c:
    
      Warning(kernel/sched/fair.c:3660): Excess function parameter 'cpus' description in 'update_sg_lb_stats'
      Warning(kernel/sched/fair.c:3806): Excess function parameter 'cpus' description in 'update_sd_lb_stats'
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/50303714.3090204@xenotime.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3901,7 +3901,6 @@ fix_small_capacity(struct sched_domain *sd, struct sched_group *group)
  * @idle: Idle status of this_cpu
  * @load_idx: Load index of sched_domain of this_cpu for load calc.
  * @local_group: Does group contain this_cpu.
- * @cpus: Set of cpus considered for load balancing.
  * @balance: Should we balance.
  * @sgs: variable to hold the statistics for this group.
  */
@@ -4051,7 +4050,6 @@ static bool update_sd_pick_busiest(struct sched_domain *sd,
 /**
  * update_sd_lb_stats - Update sched_domain's statistics for load balancing.
  * @env: The load balancing environment.
- * @cpus: Set of cpus considered for load balancing.
  * @balance: Should we balance.
  * @sds: variable to hold the statistics for this sched_domain.
  */
