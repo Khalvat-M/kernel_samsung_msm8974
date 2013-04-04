b44279b647bc73eb0f43bdaa874ab4c879ce0353
Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
2013-04-04 19:40:50 +0900

    kprobes: Move __kprobes definition into compiler.h
    Currently, __kprobes is defined in linux/kprobes.h which
    is too big to be included in small or basic headers
    that want to make use of this simple attribute.
    
    So move __kprobes definition into linux/compiler.h
    in which other compiler attributes are defined.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Cc: Timo Juhani Lindfors <timo.lindfors@iki.fi>
    Cc: Ananth N Mavinakayanahalli <ananth@in.ibm.com>
    Cc: Pavel Emelyanov <xemul@parallels.com>
    Cc: Jiri Kosina <jkosina@suse.cz>
    Cc: Nadia Yvette Chambers <nyc@holomorphy.com>
    Cc: yrl.pp-manager.tt@hitachi.com
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20130404104049.21071.20908.stgit@mhiramat-M0-7522
    [ Improved the attribute explanation a bit. ]
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -455,4 +455,10 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 	(volatile typeof(x) *)&(x); })
 #define ACCESS_ONCE(x) (*__ACCESS_ONCE(x))
 
+/* Ignore/forbid kprobes attach on very low level functions marked by this attribute: */
+#ifdef CONFIG_KPROBES
+# define __kprobes	__attribute__((__section__(".kprobes.text")))
+#else
+# define __kprobes
+#endif
 #endif /* __LINUX_COMPILER_H */
