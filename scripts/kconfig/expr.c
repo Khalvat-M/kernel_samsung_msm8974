0c78c367d60866b84197e232e8dde317770311c4
Michal Marek <mmarek@suse.cz>
2015-02-24 16:32:09 +0100

    kconfig: Remove dead code
    Signed-off-by: Michal Marek <mmarek@suse.cz>

--- scripts/kconfig/expr.c
+++ scripts/kconfig/expr.c
@@ -985,9 +985,6 @@ tristate expr_calc_value(struct expr *e)
 
 int expr_compare_type(enum expr_type t1, enum expr_type t2)
 {
-#if 0
-	return 1;
-#else
 	if (t1 == t2)
 		return 0;
 	switch (t1) {
@@ -1012,7 +1009,6 @@ int expr_compare_type(enum expr_type t1, enum expr_type t2)
 	}
 	printf("[%dgt%d?]", t1, t2);
 	return 0;
-#endif
 }
 
 static inline struct expr *
