e70468583ec6c97dc8370bd063d8434640e06fb4
Linus Torvalds <torvalds@linux-foundation.org>
2017-01-07 19:14:29 +0100

    splice: introduce FMODE_SPLICE_READ and FMODE_SPLICE_WRITE
    Introduce FMODE_SPLICE_READ and FMODE_SPLICE_WRITE. These modes check
    whether it is legal to read or write a file using splice. Both get
    automatically set on regular files and are not checked when a 'struct
    fileoperations' includes the splice_{read,write} methods.
    
    Change-Id: Ice6a3fab20bf0ac131f8d908f4bb0f7dc34bf4e3
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- fs/open.c
+++ fs/open.c
@@ -686,6 +686,10 @@ static struct file *do_dentry_open(struct dentry *dentry, struct vfsmount *mnt,
 		return f;
 	}
 
+	if (S_ISREG(inode->i_mode))
+		f->f_mode |= FMODE_SPLICE_WRITE | FMODE_SPLICE_READ;
+
+
 	f->f_op = fops_get(inode->i_fop);
 
 	error = security_file_open(f, cred);
