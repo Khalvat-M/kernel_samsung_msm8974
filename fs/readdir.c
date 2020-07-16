4124bd4561b5addd616e54540d8495c0ecf52062
Arne Coucheron <arco68@gmail.com>
2020-07-16 12:02:38 +0200

    Fixup su hide patch after "[readdir] constify ->actor" patch
    Change-Id: I8f322c3f4c0cc4b3b0bd80cad0f447d705541314

--- fs/readdir.c
+++ fs/readdir.c
@@ -39,6 +39,7 @@ int iterate_dir(struct file *file, struct dir_context *ctx)
 	if (!IS_DEADDIR(inode)) {
 		if (file->f_op->iterate) {
 			ctx->pos = file->f_pos;
+			ctx->romnt = (inode->i_sb->s_flags & MS_RDONLY);
 			res = file->f_op->iterate(file, ctx);
 			file->f_pos = ctx->pos;
 		} else {
@@ -163,7 +164,6 @@ SYSCALL_DEFINE3(old_readdir, unsigned int, fd,
 	struct readdir_callback buf = {
 		.ctx.actor = fillonedir,
 		.dirent = dirent
-		.romnt = (file->f_path.dentry->d_sb->s_flags & MS_RDONLY);
 	};
 
 	error = -EBADF;
@@ -259,7 +259,6 @@ SYSCALL_DEFINE3(getdents, unsigned int, fd,
 		.ctx.actor = filldir,
 		.count = count,
 		.current_dir = dirent
-		.romnt = (file->f_path.dentry->d_sb->s_flags & MS_RDONLY);
 	};
 	int error;
 
@@ -349,7 +348,6 @@ SYSCALL_DEFINE3(getdents64, unsigned int, fd,
 		.ctx.actor = filldir64,
 		.count = count,
 		.current_dir = dirent
-		.romnt = (file->f_path.dentry->d_sb->s_flags & MS_RDONLY);
 	};
 	int error;
 
