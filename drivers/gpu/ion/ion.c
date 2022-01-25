ced925cf892783b61073bb248060147a27e4b6b1
Daniel Rosenberg <drosen@google.com>
2022-01-25 14:18:06 +0000

    ion: Fix use after free during ION_IOC_ALLOC
    If a user happens to call ION_IOC_FREE during an ION_IOC_ALLOC
    on the just allocated id, and the copy_to_user fails, the cleanup
    code will attempt to free an already freed handle.
    
    This adds a wrapper for ion_alloc that adds an ion_handle_get to
    avoid this.
    
    Issue: SEC-3341
    Issue: SEC-3344
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Dennis Cagle <d-cagle@codeaurora.org>
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 504e1d6ee65d5b5a053253ae62f46035d774353c)
    Change-Id: Id3e558ce7bb983447752852dcdea33c65a323e45

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -524,9 +524,9 @@ static int ion_handle_add(struct ion_client *client, struct ion_handle *handle)
 	return 0;
 }
 
-struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
+struct ion_handle *__ion_alloc(struct ion_client *client, size_t len,
 			     size_t align, unsigned int heap_id_mask,
-			     unsigned int flags)
+			     unsigned int flags, bool grab_handle)
 {
 	struct ion_handle *handle;
 	struct ion_device *dev = client->dev;
@@ -627,6 +627,8 @@ struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
 		return handle;
 
 	mutex_lock(&client->lock);
+	if (grab_handle)
+		ion_handle_get(handle);
 	ret = ion_handle_add(client, handle);
 	mutex_unlock(&client->lock);
 	if (ret) {
@@ -635,6 +637,13 @@ struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
 	}
 
 	return handle;
+}
+
+struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
+			     size_t align, unsigned int heap_id_mask,
+			     unsigned int flags)
+{
+	return __ion_alloc(client, len, align, heap_id_mask, flags, false);
 }
 EXPORT_SYMBOL(ion_alloc);
 
@@ -1434,8 +1443,8 @@ static long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		if (copy_from_user(&data, (void __user *)arg, sizeof(data)))
 			return -EFAULT;
-		handle = ion_alloc(client, data.len, data.align,
-					     data.heap_mask, data.flags);
+		handle = __ion_alloc(client, data.len, data.align,
+					     data.heap_mask, data.flags, true);
 
 		if (IS_ERR(handle))
 			return PTR_ERR(handle);
@@ -1446,8 +1455,10 @@ static long ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			mutex_lock(&client->lock);
 			user_ion_free_nolock(client, handle);
 			mutex_unlock(&client->lock);
+			ion_handle_put(handle);
 			return -EFAULT;
 		}
+		ion_handle_put(handle);
 		break;
 	}
 	case ION_IOC_FREE:
