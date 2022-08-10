f5a532a12b1b917fa8f7054eb7a33f9bdf364c90
Khalvat-M <mmba313@gmail.com>
2022-08-10 22:57:45 +0430

    FIXUP: perf: Introduce a flag to enable close-on-exec in perf_event_open()
    Unlike recent modern userspace API such as:
    
      epoll_create1 (EPOLL_CLOEXEC), eventfd (EFD_CLOEXEC),
      fanotify_init (FAN_CLOEXEC), inotify_init1 (IN_CLOEXEC),
      signalfd (SFD_CLOEXEC), timerfd_create (TFD_CLOEXEC),
      or the venerable general purpose open (O_CLOEXEC),
    
    perf_event_open() syscall lack a flag to atomically set FD_CLOEXEC
    (eg. close-on-exec) flag on file descriptor it returns to userspace.
    
    The present patch adds a PERF_FLAG_FD_CLOEXEC flag to allow
    perf_event_open() syscall to atomically set close-on-exec.
    
    Having this flag will enable userspace to remove the file descriptor
    from the list of file descriptors being inherited across exec,
    without the need to call fcntl(fd, F_SETFD, FD_CLOEXEC) and the
    associated race condition between the current thread and another
    thread calling fork(2) then execve(2).
    
    Links:
    
     - Secure File Descriptor Handling (Ulrich Drepper, 2008)
       http://udrepper.livejournal.com/20407.html
    
     - Excuse me son, but your code is leaking !!! (Dan Walsh, March 2012)
       http://danwalsh.livejournal.com/53603.html
    
     - Notes in DMA buffer sharing: leak and security hole
       http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/tree/Documentation/dma-buf-sharing.txt?id=v3.13-rc3#n428
    
    Signed-off-by: Yann Droneaud <ydroneaud@opteya.com>
    Cc: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/8c03f54e1598b1727c19706f3af03f98685d9fe6.1388952061.git.ydroneaud@opteya.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [Backport to FP2: Apply change to include/linux/perf_event.h instead of
    inluce/uapi/.., which we don't have.]
    (cherry picked from commit a21b0b354d4ac39be691f51c53562e2c24443d9e)
    
    Change-Id: Ic748c50203a1b961206ef4849b6b0b107e9c190a

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1412,6 +1412,11 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
+		/*
+		 * Reload the task pointer, it might have been changed by
+		 * a concurrent perf_event_context_sched_out().
+		 */
+		task = ctx->task;
 		goto retry;
 	}
 
@@ -1864,11 +1869,6 @@ retry:
 	 */
 	if (ctx->is_active) {
 		raw_spin_unlock_irq(&ctx->lock);
-		/*
-		 * Reload the task pointer, it might have been changed by
-		 * a concurrent perf_event_context_sched_out().
-		 */
-		task = ctx->task;
 		/*
 		 * Reload the task pointer, it might have been changed by
 		 * a concurrent perf_event_context_sched_out().
